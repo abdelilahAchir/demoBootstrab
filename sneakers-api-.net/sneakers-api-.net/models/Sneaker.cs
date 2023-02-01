397882bc2f7acde846f405491b1b640d296c7ffe
Abdelilah Achir <abdiachir@gmail.com>
2023-02-01 16:23:43 +0100

    first commit

---
+++ sneakers-api-.net/sneakers-api-.net/models/Sneaker.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace sneakers_api_.net.models
+{
+    public class Sneaker
+    {
+        public string brand { get; set; }
+        public string model { get; set; }
+        public string price { get; set; }
+        public string[] sizes { get; set; }
+        public string gender { get; set; }
+        public string[] colors { get; set; }
+        public string[] images_urls { get; set; }
+    }
+}
---
+++ sneakers-api-.net/sneakers-api-.net/models/Sneakers.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json;
+
+namespace sneakers_api_.net.models
+{
+    public class Sneakers
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        private readonly Dictionary<int, Sneaker> _sneakers = new Dictionary<int, Sneaker>();
+
+        public Sneakers()
+        {
+            FetchAllSneakers().Wait();
+        }
+        public async Task FetchAllSneakers()
+        {
+
+           
+                var response = await _httpClient.GetAsync($"https://sneakersxu.azurewebsites.net/sneakers");
+                var json = await response.Content.ReadAsStringAsync();
+                var data = JsonSerializer.Deserialize<JsonElement>(json);
+
+                for (var i = 0; i < data.GetArrayLength(); i++)
+                {
+                    var brand = data[i].GetProperty("brand").GetString();
+                    var model = data[i].GetProperty("model").GetString();
+                    var price = data[i].GetProperty("price").GetString();
+
+                    var sizes = new List<string>();
+                    for (var j = 0; j < data[i].GetProperty("sizes").GetArrayLength(); j++)
+                    {
+                        var size = data[i].GetProperty("sizes")[j].GetString();
+                        if (size != null) sizes.Add(size);
+                    }
+
+                    var gender = data[i].GetProperty("gender").GetString();
+
+                    var colors = new List<string>();
+                    for (var j = 0; j < data[i].GetProperty("colors").GetArrayLength(); j++)
+                    {
+                        var color = data[i].GetProperty("colors")[j].GetString();
+                        if (color != null) colors.Add(color);
+                    }
+
+                    var images_urls = new List<string>();
+                    for (int j = 0; j < data[i].GetProperty("images_urls").GetArrayLength(); j++)
+                    {
+                        var image_url = data[i].GetProperty("images_urls")[j].GetString();
+                        if(image_url != null) images_urls.Add(image_url);
+                    }
+
+                    var sneaker = new Sneaker
+                    {
+                        brand = brand, model = model, price = price, sizes = sizes.ToArray(), gender = gender,
+                        colors = colors.ToArray(), images_urls = images_urls.ToArray()
+                    };
+
+                    _sneakers.Add(i,sneaker);
+
+                }
+                
+        }
+        public List<Sneaker> GetAllSneakers() => _sneakers.Values.ToList();
+
+    }
+}
