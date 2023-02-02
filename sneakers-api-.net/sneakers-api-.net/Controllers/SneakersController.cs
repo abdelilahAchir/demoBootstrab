2d51f4c9c5e261725f6c402c0e526e5688cc0fce
Abdelilah Achir <abdiachir@gmail.com>
2023-02-02 08:15:07 +0100

    refactored some code

--- sneakers-api-.net/sneakers-api-.net/Controllers/SneakersController.cs
+++ sneakers-api-.net/sneakers-api-.net/Controllers/SneakersController.cs
@@ -1,55 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using sneakers_api_.net.models;
-using System.Net.Http;
 using Newtonsoft.Json;
+using sneakers_api_.net.models;
 using System.Text;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace sneakers_api_.net.Controllers
 {
-    [Route("[controller]")]
-    [ApiController]
-    
-    public class SneakersController : ControllerBase
-    {
+	[Route("[controller]")]
+	[ApiController]
 
-        private readonly HttpClient _httpClient;
+	public class SneakersController : ControllerBase
+	{
 
-        public SneakersController(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-            _httpClient.BaseAddress = new System.Uri("https://sneakersxu.azurewebsites.net/");
-        }
-        // GET: api/<SneakersController>
-        [HttpGet]
-        public async Task<IActionResult> Get(Sneakers sneakerList )
-        {
-            var response = await _httpClient.GetAsync("sneakers");
+		private readonly HttpClient _httpClient;
 
-            if (response.IsSuccessStatusCode)
-            {
-              var snkrs =  sneakerList.GetAllSneakers();
-                //var data = await response.Content.ReadAsStringAsync();
-                //var sneakerList = JsonConvert.DeserializeObject<List<Sneakers>>(data);
-                return Ok(snkrs);
-            }
-            else
-            {
-                return StatusCode(500, "Request to external API failed with status code: " + response.StatusCode);
-            }
-        }
+		public SneakersController(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+			_httpClient.BaseAddress = new System.Uri("https://sneakersxu.azurewebsites.net/");
+		}
+		// GET: api/<SneakersController>
+		[HttpGet]
+		public async Task<IActionResult> Get(Sneakers sneakerList)
+		{
+			var response = await _httpClient.GetAsync("sneakers");
+
+			if (response.IsSuccessStatusCode)
+			{
+				await sneakerList.FetchAllSneakers();
+				var sneakers = sneakerList.GetAllSneakers();
+				return Ok(sneakers);
+			}
+			else
+			{
+				return StatusCode(500, "Request to external API failed with status code: " + response.StatusCode);
+			}
+		}
 
 
-        // POST api/<SneakersController>
-        [HttpPost]
-        public async Task<IActionResult> Post(Sneaker sneaker)
-        {
+		// POST api/<SneakersController>
+		[HttpPost]
+		public async Task<IActionResult> Post(Sneaker sneaker)
+		{
 			var json = JsonConvert.SerializeObject(sneaker);
 			var content = new StringContent(json, Encoding.UTF8, "application/json");
 			var response = await _httpClient.PostAsync("https://sneakersxu.azurewebsites.net/addSneakerXu", content);
 
-			if (!response.IsSuccessStatusCode)
+			if (response.IsSuccessStatusCode)
 			{
 				return Ok();
 			}
@@ -58,22 +56,22 @@ namespace sneakers_api_.net.Controllers
 
 		}
 
-        
 
-        // DELETE api/<SneakersController>/5
-        [HttpDelete("sneaker")]
-        public async Task<IActionResult> Delete(Sneaker sneaker)
-        {
-	        var json = JsonConvert.SerializeObject(sneaker);
-	        var content = new StringContent(json, Encoding.UTF8, "application/json");
-	        var response = await _httpClient.PostAsync("https://sneakersxu.azurewebsites.net/deleteSneakerXu", content);
 
-	        if (response.IsSuccessStatusCode)
-	        {
-		        return Ok();
-	        }
+		// DELETE api/<SneakersController>/5
+		[HttpDelete]
+		public async Task<IActionResult> Delete(Sneaker sneaker)
+		{
+			var json = JsonConvert.SerializeObject(sneaker);
+			var content = new StringContent(json, Encoding.UTF8, "application/json");
+			var response = await _httpClient.PostAsync("https://sneakersxu.azurewebsites.net/deleteSneakerXu", content);
 
-	        return BadRequest();
+			if (response.IsSuccessStatusCode)
+			{
+				return Ok();
+			}
+
+			return BadRequest();
 		}
-    }
+	}
 }
--- sneakers-api-.net/sneakers-api-.net/models/Sneaker.cs
+++ sneakers-api-.net/sneakers-api-.net/models/Sneaker.cs
@@ -1,17 +1,14 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.DataProtection;
-
-namespace sneakers_api_.net.models
+﻿namespace sneakers_api_.net.models
 {
-    public class Sneaker
-    {
-        public string brand { get; set; }
-        public string model { get; set; }
-        public string price { get; set; }
-        public string[] sizes { get; set; }
-        public string gender { get; set; }
-        public string[] colors { get; set; }
-        public string[] images_urls { get; set; }
-    }
+	public class Sneaker
+	{
+		public int id { get; set; }
+		public string brand { get; set; }
+		public string model { get; set; }
+		public string price { get; set; }
+		public string[] sizes { get; set; }
+		public string gender { get; set; }
+		public string[] colors { get; set; }
+		public string[] images_urls { get; set; }
+	}
 }
