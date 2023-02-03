50478cdd5edbfcea0d49959da3f973689fae5b5e
Abdelilah Achir <abdiachir@gmail.com>
2023-02-03 09:31:23 +0100

    Added more conditions before adding or deleting a object

--- sneakers-api-.net/sneakers-api-.net/Controllers/SneakersController.cs
+++ sneakers-api-.net/sneakers-api-.net/Controllers/SneakersController.cs
@@ -12,7 +12,6 @@ namespace sneakers_api_.net.Controllers
 
 	public class SneakersController : ControllerBase
 	{
-
 		private readonly HttpClient _httpClient;
 
 		public SneakersController(HttpClient httpClient)
@@ -38,19 +37,17 @@ namespace sneakers_api_.net.Controllers
 			}
 		}
 
-
 		// POST api/<SneakersController>
 		[HttpPost]
 		public async Task<IActionResult> Post(Sneaker sneaker,Sneakers _sneakers)
 		{
 			var allSneakers = _sneakers.GetAllSneakers();
-			 var existingSneaker = allSneakers.Find(s => s.id == sneaker.id);
+			var existingSneaker = allSneakers.Find(s => s.id == sneaker.id && s.brand == sneaker.brand && s.model == sneaker.model);
 			if (existingSneaker == null)
 			{
 				var json = JsonConvert.SerializeObject(sneaker);
 				var content = new StringContent(json, Encoding.UTF8, "application/json");
 				var response = await _httpClient.PostAsync("https://sneakersxu.azurewebsites.net/addSneakerXu", content);
-
 				if (response.IsSuccessStatusCode)
 				{
 					return Ok();
@@ -60,22 +57,24 @@ namespace sneakers_api_.net.Controllers
 			return BadRequest("the id of the sneakers have to be unique");
 		}
 
-
-
 		// DELETE api/<SneakersController>/5
 		[HttpDelete]
-		public async Task<IActionResult> Delete(Sneaker sneaker)
+		public async Task<IActionResult> Delete(Sneaker sneaker, Sneakers _sneakers)
 		{
-			var json = JsonConvert.SerializeObject(sneaker);
-			var content = new StringContent(json, Encoding.UTF8, "application/json");
-			var response = await _httpClient.PostAsync("https://sneakersxu.azurewebsites.net/deleteSneakerXu", content);
-
-			if (response.IsSuccessStatusCode)
+			var allSneakers = _sneakers.GetAllSneakers();
+			var existingSneaker = allSneakers.Find(s => s.id == sneaker.id && s.brand == sneaker.brand && s.model == sneaker.model);
+			if ( existingSneaker != null)
 			{
-				return Ok();
+				var json = JsonConvert.SerializeObject(sneaker);
+				var content = new StringContent(json, Encoding.UTF8, "application/json");
+				var response = await _httpClient.PostAsync("https://sneakersxu.azurewebsites.net/deleteSneakerXu", content);
+				if (response.IsSuccessStatusCode)
+				{
+					return Ok();
+				}
+				return BadRequest();
 			}
-
-			return BadRequest();
+			return BadRequest("The object that you request doesn't exist");
 		}
 	}
 }
